efa88a81ebff7632116e2fe51548fcf6f5f62f98
Pablo Maidana <jpabmai@gmail.com>
2020-04-23 14:28:15 -0300

    Creacion de Modelo Usuario
    Conflicts:
    	Models/Models.csproj
    	Models/Properties/AssemblyInfo.cs
    	Services/Properties/AssemblyInfo.cs
    	Services/Services.csproj

--- Models/Data/BaseDatos.cs
+++ Models/Data/BaseDatos.cs
@@ -8,5 +8,21 @@ namespace Models.Data
 {
     class BaseDatos
     {
+        static private Usuario usuario { get; set; }
+    }
+
+    class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string TipoUsuario { get; set; }
+
+        public DateTime FechaNacicmiento { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+        public bool Activo { get; set; }
     }
 }
