529ed8176c83a4b26bbc26d5e99632f5987310c5
Pablo Maidana <jpabmai@gmail.com>
2020-04-24 23:19:39 -0300

    Se creo ViewModel Registrar e Ingresar

---
+++ Models/ViewModels/Necesidad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.ViewModels
+{
+    public class Necesidad
+    {
+        public string Titulo { get; set; }
+        public string PathFoto { get; set; }
+        public string NombreUsuario { get; set; }
+        public int Valoracion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string LinkDetalle { get; set; }
+    }
+}
---
+++ Models/ViewModels/Registrar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    class Registrar
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RePassword { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
