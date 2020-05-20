71e996c3561db0a943133957fbe327d44b140e01
Ezequiel Paoloni <ezequielpaoloni@outlook.com>
2020-05-20 17:37:39 -0300

    Modificaciones en archivos

---
+++ WebApp/Controllers/PerfilController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class PerfilController : Controller
+    {
+        // GET: Perfil
+        public ActionResult Inicio()
+        {
+            return RedirectToAction("Datos");
+        }
+        public ActionResult Datos()
+        {
+            return View();
+        }
+
+        public ActionResult MisDonaciones()
+        {
+            return View();
+        }
+    }
+}
