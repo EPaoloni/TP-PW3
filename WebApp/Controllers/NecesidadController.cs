d6683f2e7e9e4bad6e97550dd447c4cc1b97ee78
Ezequiel Paoloni <ezequielpaoloni@outlook.com>
2020-05-20 17:37:18 -0300

    Agrego la pantalla de necesidad

---
+++ WebApp/Controllers/NecesidadController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class NecesidadController : Controller
+    {
+        // GET: Perfil
+        public ActionResult Inicio()
+        {
+            return RedirectToAction("CrearNecesidad");
+        }
+        public ActionResult CrearNecesidad()
+        {
+            return View();
+        }
+    }
+}
