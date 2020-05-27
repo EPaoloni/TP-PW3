358c74924e9bf02f1b106a2e8976451a1c11d66a
Pablo Maidana <jpmaidanai@unla.edu.ar>
2020-05-27 11:00:22 -0300

    Creacion de validacion Existe Email

--- Models/Partial/UsuariosPartial.cs
+++ Models/Partial/UsuariosPartial.cs
@@ -12,6 +12,8 @@ namespace Models.Partial
         public int IdUsuario { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
+        [Required(ErrorMessage = "Campo requerido")]
+        [Range(18,99,ErrorMessage = "Debe ser mayor de 18 años")]
         public System.DateTime FechaNacimiento { get; set; }
         public string UserName { get; set; }
         [Required(ErrorMessage = "Campo requerido")]
@@ -19,11 +21,25 @@ namespace Models.Partial
         public string Email { get; set; }
         [Required(ErrorMessage = "Campo requerido")]
         public string Password { get; set; }
+        public string RepetirPassword { get; set; }
         public string Foto { get; set; }
         public int TipoUsuario { get; set; }
-        public System.DateTime FechaCracion { get; set; }
+        public System.DateTime FechaCrecion { get; set; }
         public bool Activo { get; set; }
         public string Token { get; set; }
         public bool RespuestaLogin { get; set; } = false;
+    }
+
+    public class EmailExisteAttribute : ValidationAttribute
+    {
+        List<string> listaEmail = new List<string>()
+        {
+            "jpabmai@gmail.com"
+        };
+
+        public override bool IsValid(object value)
+        {
+            return listaEmail.Contains(value);
+        }
     }
 }
