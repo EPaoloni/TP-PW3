7904b5d89b78c69337743813fbb3a2e2052796be
PabMai <jpabmai@gmail.com>
2020-06-08 01:12:15 -0300

    Generacion de nombre de usuario

--- Services/Home/UsuarioService.cs
+++ Services/Home/UsuarioService.cs
@@ -1,4 +1,5 @@
-﻿using Models.Partial;
+﻿using Models.ORM;
+using Models.Partial;
 using Models.Repository;
 using System;
 using System.Collections.Generic;
@@ -12,11 +13,13 @@ namespace Services.Home
     public class UsuarioService
     {
         EmailService emailServ = new EmailService();
-        RegistrarseRepository usuarioRepo = new RegistrarseRepository();
-        
+        RegistrarseRepository regRepo = new RegistrarseRepository();
+        PerfilRepository perfilRepo = new PerfilRepository();
+
+
         public void Activar(string token)
         {
-            usuarioRepo.Activar(token);
+            regRepo.Activar(token);
         }
 
         public void Agregar(RegistrarseMetaData usuario)
@@ -25,10 +28,27 @@ namespace Services.Home
             usuario.Token = Guid.NewGuid().ToString("N").Substring(0, 16);
 
             // Se agrega el usuario en base de datos
-            usuarioRepo.Agregar(usuario);
+            regRepo.Agregar(usuario);
 
             // Se envia el email de activacion
             emailServ.Enviar(usuario.Email, usuario.Token);
         }
+
+        public void GenerarNombreUsuario(string nombre, string apellido)
+        {
+            string nombreUsuario = "";
+
+            int cantUsuario = perfilRepo.ValidarNombreUsuario(nombre, apellido);
+
+            if (cantUsuario > 0)
+            {
+                nombreUsuario = nombre + "." + apellido + cantUsuario;
+            }
+            else
+            {
+                nombreUsuario = nombre + "." + apellido;
+            }
+        }
+
     }
 }
