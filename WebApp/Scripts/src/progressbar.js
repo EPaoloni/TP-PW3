a1a7f8b522742ff5ae6ca59fa58343f33c950060
PabMai <jpabmai@gmail.com>
2020-06-17 23:49:45 -0300

    Cambios en progressbar

--- WebApp/Scripts/src/progressbar.js
+++ WebApp/Scripts/src/progressbar.js
@@ -1,17 +1,4 @@
 ﻿$(document).ready(function () {
-    function animateProgressBar(percentageCompleted)
-    {
-        const WIDTH_BACKGROUND = 500
-
-        ('#progressbar-inner').animate({
-            'width': (WIDTH_BACKGROUND * percentageCompleted) / 100
-        }, 3000);
-
-        $({ counter: 1 }).animate({ counter: percentageCompleted }, {
-            duration: 3000,
-            step: function () {
-                $('#progressbar-inner').text(Math.ceil(this.counter) + ' %')
-            }
-        })
-    }   
-})
+    var inner = $('#progressbar-inner');
+    $(".progress .determinate").css(width, inner);
+});
