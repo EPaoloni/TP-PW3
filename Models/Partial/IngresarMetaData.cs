77620685c81b8577b3b27fd0b1a67501c02b56e3
Ezequiel Paoloni <ezequielpaoloni@outlook.com>
2020-07-01 18:44:13 -0300

    Merge Models/Partial

---
+++ Models/Partial/IngresarMetaData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.Partial
+{
+
+    public class IngresarMetaData
+    {
+        [Required(ErrorMessage = "Campo requerido")]
+        [EmailAddress(ErrorMessage = "Formato de email incorrecto")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", ErrorMessage = "Formato de password incorrecto")]
+        public string Password { get; set; }
+        public bool RespuestaLogin { get; set; } = false;
+    }
+}
---
+++ Models/Partial/PerfilMetaData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Models.Partial
+{
+    public class PerfilMetaData
+    {
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellido { get; set; }
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [MayorEdad(ErrorMessage = "Debe ser mayor de 18 años")]
+        public DateTime FechaNacimiento { get; set; }
+        public string RutaFoto { get; set; }
+        [Required]
+        public HttpPostedFileBase Archivo { get;set; }
+        public string Email { get; set; }
+        public string NombreUsuario { get; set; }
+    }
+}
