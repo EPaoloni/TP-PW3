e44433cdf38fe56553f134ae6d065ea450ee008a
PabMai <jpabmai@gmail.com>
2020-07-02 11:21:38 -0300

    Inicio de Mis Necesidades

---
+++ WebApp/Controllers/MisNecesidadesController.cs
@@ -0,0 +1,21 @@
+﻿using Services.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class MisNecesidadesController : Controller
+    {
+
+        NecesidadService necesidadSrv = new NecesidadService();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
